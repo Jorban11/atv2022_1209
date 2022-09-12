b5ff230211603ff3ac8509fd89bd63f281c3d92c
João Carlos Orban dos Santos - 818117293 <818117293@ulife.com.br>
2022-09-12 20:20:08 -0300

    usuarios

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+
+const usuarios = {}//Usuários têm id, nome e e-mail.
+
+app.get('rede/usuarios',(req,res) => {
+    res.send(usuarios)
+})
+
+app.post('rede/usuarios',(req,res) => {
+    cont++
+
+    const id = req.body.id
+    const nome = req.body.nome
+    const email = req.body.email
+
+    usuarios[cont] = {
+        id,
+        nome,
+        email
+    }
+
+    res.status(400).end()
+})
+
+app.listen(2000,(req,res) => {
+    console.log('Porta 2000 (----------------------------------)')
+})
